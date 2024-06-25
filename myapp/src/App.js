d45960cd365b6b78cbe7dd73f75261c2e0d9e5c6
Lohi26 <lohitachandru@gmail.com>
2024-06-25 14:33:48 +0530

    JavaScript XML

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Second from './Components/Second';
 import Starting from './Components/Starting';
 import ClassDemo from './Components/ClassDemo';
+import Jsx from './Components/Jsx';
 function App() {
   return (
     <div>
       <Starting/>
       <Second/>
       <ClassDemo/>
+      <Jsx/>
     </div>
   );
 }
---
+++ myapp/src/Components/Jsx.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Jsx()
+{
+    return React.createElement(
+        'div',
+        null,
+        React.createElement('h1',null,'Hello World!!')
+    );
+}
+export default Jsx;
+
+//without JSX!!
